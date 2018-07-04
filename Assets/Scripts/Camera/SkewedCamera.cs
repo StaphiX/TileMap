34d1ade78c203189e3c1c8d345daf176834c2a73
StaphiX <superdan_1990@hotmail.co.uk>
2018-07-04 19:32:37 +0100

    Added a scene to test camera projections
    Possibly able to keep scene logic 3D but render as if it was 2D

---
+++ Assets/Scripts/Camera/SkewedCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is meant to be attached to your main camera.
+// If you want to use it on more than one camera at a time, it will require
+// modifcations due to the Camera.on* delegates in OnEnable()/OnDisable().
+
+[ExecuteInEditMode]
+public class SkewedCamera : MonoBehaviour
+{
+    public static int PIXELSPERUNIT = 64;
+    public static int PIXELSCALE = 1;
+
+    private void Awake()
+    {
+        SetupCamera();
+    }
+
+    void SetupCamera()
+    {
+        Camera.main.orthographicSize = ((float)Screen.height / ((float)PIXELSCALE * (float)PIXELSPERUNIT)) * 0.5f;
+        //SetObliqueness(0, -1);
+    }
+
+    void SetObliqueness(float horizObl, float vertObl)
+    {
+        Matrix4x4 mat = Camera.main.projectionMatrix;
+        mat[0, 2] = horizObl;
+        mat[1, 2] = vertObl;
+        Camera.main.projectionMatrix = Camera.main.projectionMatrix.
+    }
+
+    private void OnPreCull()
+    {
+        SetObliqueness(0.0f, 0.25f);
+    }
+}
